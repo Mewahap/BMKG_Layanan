387d9c351840055ce1d63d5968fb486558c4a7a2
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-08-03 12:31:18 +0700

    update7

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+function whatsapp() {
+  var name = document.getElementById("name").value;
+  var mobile = document.getElementById("mobile").value.replace(/\s+/g, ""); // Remove spaces from mobile number
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  //   var whatsappurl = "https://wa.me/+628112562200?text=" + "Nama: " + name + "%0a" + "Nomor Telp: " + mobile + "%0a" + "Email: " + email + "%0a" + "Pesan: " + message;
+  var whatsappurl = "https://wa.me/+62895383243075?text=" + "Nama: " + name + "%0a" + "Nomor Telp: " + mobile + "%0a" + "Email: " + email + "%0a" + "Pesan: " + message;
+
+  window.open(whatsappurl, "_blank").focus();
+}
+
+// Close the modal if the user clicks anywhere outside of it
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
