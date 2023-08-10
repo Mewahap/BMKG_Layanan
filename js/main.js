532d2c8fa7f5b73eb090ac3d37570920e8db97cf
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-08-10 10:12:33 +0700

    update11

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function whatsapp() {
   var message = document.getElementById("message").value;
 
   //   var whatsappurl = "https://wa.me/+628112562200?text=" + "Nama: " + name + "%0a" + "Nomor Telp: " + mobile + "%0a" + "Email: " + email + "%0a" + "Pesan: " + message;
-  var whatsappurl = "https://eskm.bmkg.go.id/survey/436860/0/2/2022-07/2022/0" + "Nama: " + name + "%0a" + "Nomor Telp: " + mobile + "%0a" + "Email: " + email + "%0a" + "Pesan: " + message;
+  var whatsappurl = "https://wa.me/+62895383243075?text=" + "https://eskm.bmkg.go.id/survey/436860/0/2/2022-07/2022/0" + "Nama: " + name + "%0a" + "Nomor Telp: " + mobile + "%0a" + "Email: " + email + "%0a" + "Pesan: " + message;
 
   window.open(whatsappurl, "_blank").focus();
 }
